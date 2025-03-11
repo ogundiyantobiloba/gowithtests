05a50531bfef9532a3ff96fd25a62537cc90c6e9
Tobiloba Ogundiyan <114788338+ogundiyantobiloba@users.noreply.github.com>
2025-03-11 13:58:11 +0000

    added helper function for test assertions

--- hello_test.go
+++ hello_test.go
@@ -9,19 +9,22 @@ func TestHello(t *testing.T) {
 
 		got := Hello("Chris")
 		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
