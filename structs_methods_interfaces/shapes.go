455e80f907aaed89e9e67492fac60b0fa5161350
Tobiloba Ogundiyan <114788338+ogundiyantobiloba@users.noreply.github.com>
2025-03-16 14:08:49 +0000

    added interfaces

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -1,14 +1,23 @@
 package structs_methods_interfaces
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
-
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Circle struct {
+	Radius float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Shape interface {
+	Area() float64
 }
