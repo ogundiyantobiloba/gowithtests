cf8ed5f46ae206dad9b54bf734138ac828753c73
Tobiloba Ogundiyan <114788338+ogundiyantobiloba@users.noreply.github.com>
2025-03-16 14:23:28 +0000

    completed structs method and interfaces

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -10,6 +10,15 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
